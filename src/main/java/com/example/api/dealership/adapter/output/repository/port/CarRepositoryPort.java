508c2dc6a59b3f4a0fdd22c2c9c3c2d1aa3f689c
João Victor <joaovsg@id.uff.br>
2023-05-26 17:02:39 -0300

    [Feature] - Adapting the repository ports to use specifications

--- src/main/java/com/example/api/dealership/adapter/output/repository/port/CarRepositoryPort.java
+++ src/main/java/com/example/api/dealership/adapter/output/repository/port/CarRepositoryPort.java
@@ -2,12 +2,13 @@ package com.example.api.dealership.adapter.output.repository.port;
 
 import com.example.api.dealership.core.domain.CarModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 @Repository
-public interface CarRepositoryPort extends JpaRepository<CarModel, UUID> {
+public interface CarRepositoryPort extends JpaRepository<CarModel, UUID>, JpaSpecificationExecutor<CarModel> {
 
     Optional<CarModel> findBycarVin(String vehicleIdentificationNumber);
 
--- src/main/java/com/example/api/dealership/adapter/output/repository/port/SalesRepositoryPort.java
+++ src/main/java/com/example/api/dealership/adapter/output/repository/port/SalesRepositoryPort.java
@@ -2,8 +2,9 @@ package com.example.api.dealership.adapter.output.repository.port;
 
 import com.example.api.dealership.core.domain.SalesModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SalesRepositoryPort extends JpaRepository<SalesModel, String> {
+public interface SalesRepositoryPort extends JpaRepository<SalesModel, String>, JpaSpecificationExecutor<SalesModel> {
 }
