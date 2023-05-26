e47e6f8318f31f4180087df72f0be8cd0879383b
João Victor <joaovsg@id.uff.br>
2023-05-26 17:03:16 -0300

    [Feature] - Updating the exception handler and pom

--- src/main/java/com/example/api/dealership/adapter/mapper/UserMapper.java
+++ src/main/java/com/example/api/dealership/adapter/mapper/UserMapper.java
@@ -2,9 +2,7 @@ package com.example.api.dealership.adapter.mapper;
 
 import com.example.api.dealership.adapter.dtos.user.UserDtoRequest;
 import com.example.api.dealership.core.domain.UserModel;
-import de.mkammerer.argon2.Argon2Factory;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
