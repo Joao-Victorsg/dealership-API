0a7caf7be216865410581750b8f32edc4ecefa12
João Victor <joaovsg@id.uff.br>
2023-12-21 21:38:29 -0300

    [Feature] - Adding observability with ELK

--- src/main/java/com/example/api/dealership/DealershipApplication.java
+++ src/main/java/com/example/api/dealership/DealershipApplication.java
@@ -1,5 +1,6 @@
 package com.example.api.dealership;
 
+import co.elastic.apm.attach.ElasticApmAttacher;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +13,7 @@ public class DealershipApplication {
 
 	public static void main(String[] args) {
 		log.info("Iniciando aplicação");
+		ElasticApmAttacher.attach();
 		SpringApplication.run(DealershipApplication.class, args);
 	}
 
